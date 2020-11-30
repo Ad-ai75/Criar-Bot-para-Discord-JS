c3bcf7a5bfcfac7d17c1629ad0186bb423b068ea
Nathan Lucca Covre <42450935+nathan-lucca@users.noreply.github.com>
2020-11-30 15:04:27 -0300

    Novos Eventos

---
+++ src/eventos/client/guildMemberAdd.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const moment = require('moment');
+moment.locale('pt-BR');
+
+module.exports = async (bot, member) => {
+	const canal = member.guild.channels.cache.get('764504459362566144');
+
+	const embed = new Discord.MessageEmbed()
+		.setTitle('SEJA BEM-VINDO!')
+		.setDescription(`Seja bem-vindo ao servidor **${member.guild.name}**, \`${member.user.tag}\``)
+		.addField(`Membro:`, `${member.user.username}`)
+		.addField(`ID:`, `${member.user.id}`)
+		.addField(`Data da Entrada:`, moment(member.joinedAt).format('ll'))
+		.setFooter(`Usuário: ${member.user.username}`, member.user.displayAvatarURL({ dynamic: true }))
+		.setTimestamp();
+
+	canal.send(embed);
+};
